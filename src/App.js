d61598ef7d755ff44b92df1c421bba2492bfd368
Akshat jain <81185577+iamakj1110@users.noreply.github.com>
2022-07-07 21:58:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -23,9 +23,6 @@ function App() {
         <Navbar/>
         <div className='container'>
         <Switch>
-//           <Route exact path="/about">
-//            <About/>
-//           </Route>
           <Route exact path="/">
             <Home/>
           </Route>
